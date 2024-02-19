36e190c209c8ae5edb7bf006ab5cf2a33b6c84b7
Maid Petrović <126613894+rioDevv@users.noreply.github.com>
2024-02-19 15:04:29 +0100

    Add files via upload

---
+++ VJEZBA 8/Zadatak 2.cpp
@@ -0,0 +1,53 @@
+﻿#include<iostream>
+#include<iomanip>
+using namespace std;
+
+	/*
+		Napisati program koji provjerava da li je 1D niz rastući.
+		Velicina niza je 5.
+	*/
+
+void unos(int niz[], int vel) {
+	for (int i = 0; i < vel; i++)
+	{
+		cout << "Unesi element na indexu " << i << " : ";
+		cin >> niz[i];
+	}
+	cout << endl;
+}
+
+void ispis(int niz[], int vel) {
+	for (int i = 0; i < vel; i++)
+	{
+		cout << niz[i] << endl;
+	}
+}
+
+bool jeRastuci(int niz[], int vel) {
+	for (int i = 0; i < vel - 1; i++)
+	{
+		if (niz[i] > niz[i + 1])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	const int vel = 5;
+	int niz[vel];
+
+	unos(niz, vel);
+	ispis(niz, vel);
+
+	if (jeRastuci(niz,vel))
+	{
+		cout << "Niz je rastuci." << endl;
+	}
+	else
+	{
+		cout << "Niz nije rastuci." << endl;
+	}
+}
+
