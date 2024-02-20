e368a6c0cc64229f9675907856bab9f9f33fe6e4
Maid Petrović <126613894+rioDevv@users.noreply.github.com>
2024-02-20 23:15:53 +0100

    Add files via upload

---
+++ VJEZBA 14/Zadatak 6.cpp
@@ -0,0 +1,60 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    const int brojTekstova = 3;
+    int ukupnaVelicina = 0;
+    char** tekstovi = new char* [brojTekstova];
+
+    for (int i = 0; i < brojTekstova; i++)
+    {
+        char temp[200] = "";
+        cout << "Unesite tekst " << i + 1 << " : ";
+        cin.getline(temp, sizeof(temp));
+        int size = strlen(temp) + 1;
+        tekstovi[i] = new char[size];
+        strcpy_s(tekstovi[i], size, temp);
+        ukupnaVelicina += strlen(temp) + 1;
+    }
+
+    char* noviTekst = new char[ukupnaVelicina];
+    noviTekst[0] = '\0';
+
+    for (int i = 0; i < brojTekstova; i++)
+    {
+        strcat_s(noviTekst, ukupnaVelicina, tekstovi[i]);
+        if (i != brojTekstova - 1)
+        {
+            strcat_s(noviTekst, ukupnaVelicina, " ");
+        }
+    }
+
+    // Postavljanje svakog prvog slova u rečenici kao veliko slovo
+    bool prvoSlovo = true;
+    for (int i = 0; i < ukupnaVelicina; i++) {
+        if (isalpha(noviTekst[i])) {
+            if (prvoSlovo) {
+                noviTekst[i] = toupper(noviTekst[i]);
+                prvoSlovo = false;
+            }
+        }
+        else {
+            prvoSlovo = true;
+        }
+    }
+
+    cout << "Rezultat: " << noviTekst << endl;
+
+    delete[] noviTekst;
+
+    for (int i = 0; i < brojTekstova; i++)
+    {
+        delete[] tekstovi[i];
+        tekstovi[i] = nullptr;
+    }
+    delete[] tekstovi;
+    tekstovi = nullptr;
+
+    return 0;
+}
