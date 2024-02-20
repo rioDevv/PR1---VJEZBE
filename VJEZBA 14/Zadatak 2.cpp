8da72d35f1feb0ac46981e517e80bcbfa8c3a99a
Maid Petrović <126613894+rioDevv@users.noreply.github.com>
2024-02-20 17:20:48 +0100

    Add files via upload

---
+++ VJEZBA 14/Zadatak 2.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	char temp[200] = "";
+
+	cout << "Unesite tekst: ";
+	cin.getline(temp, size(temp));
+
+	int size = strlen(temp) + 1;
+	char* tekst = new char[size];
+
+	strcpy_s(tekst, size, temp);
+
+	int currentLongest = 0;
+	int longest = 0;
+
+	char* p = nullptr;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (isalpha(tekst[i]))
+		{
+			currentLongest++;
+		}
+		else
+		{
+			if (currentLongest > longest)
+			{
+				longest = currentLongest;
+				p = tekst + i - currentLongest;
+			}
+			currentLongest = 0;
+		}
+	}
+
+	cout << "Najveca duzina je: " << longest << endl;
+	cout << "Najduza rijec: ";
+	for (int i = 0; i < longest; i++)
+	{
+		cout << p[i];
+	}
+
+	delete[] tekst;
+	tekst = nullptr;
+}
