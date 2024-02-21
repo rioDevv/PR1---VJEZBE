462370f2821450ee356ed9e446e5357e144c4b54
Maid Petrović <126613894+rioDevv@users.noreply.github.com>
2024-02-21 17:09:50 +0100

    Add files via upload

---
+++ ZADACI ZA VJEŽBU/Zadatak 1.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+using namespace std;
+
+int nthFibonacci(int broj) {
+	if (broj <= 2)
+	{
+		return 1;
+	}
+	return nthFibonacci(broj - 1) + nthFibonacci(broj - 2);
+}
+
+int main() {
+	int broj;
+	do
+	{
+		cout << "Unesite broj: ";
+		cin >> broj;
+	} while (broj < 1);
+
+	cout << "Odgovarujuci Fibonaccijev broj je: " << nthFibonacci(broj);
+}
