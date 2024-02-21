c13b160f4c3a47cf5481f10d5f5613425374324c
Maid Petrović <126613894+rioDevv@users.noreply.github.com>
2024-02-21 18:26:25 +0100

    Add files via upload

---
+++ ZADACI ZA VJEŽBU/Zadatak 4.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+using namespace std;
+
+bool isPrime(int broj, int i) {
+	if (broj <= 1)
+	{
+		return false;
+	}
+	if (broj == 2 || broj == 3)
+	{
+		return true;
+	}
+	if (broj % i == 0)
+	{
+		return false;
+	}
+	if (i == broj - 1)
+	{
+		return true;
+	}
+	return isPrime(broj, i + 1);
+}
+
+int main() {
+	int broj;
+	do
+	{
+		cout << "Unesi broj: ";
+		cin >> broj;
+	} while (broj < 0);
+
+	if (isPrime(broj, 2))
+	{
+		cout << "Broj je prost." << endl;
+	}
+	else
+	{
+		cout << "Broj je slozen." << endl;
+	}
+}
