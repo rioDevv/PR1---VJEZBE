6889b5905003eec5dacbdb3f5dcc4319a8788e21
Maid Petrović <126613894+rioDevv@users.noreply.github.com>
2024-02-21 17:10:49 +0100

    Add files via upload

---
+++ ZADACI ZA VJEŽBU/Zadatak 2.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+using namespace std;
+
+void printLettersInReverse(char a, char b) {
+	if (!isupper(a) || !isupper(b))
+	{
+		return;
+	}
+	if (a > b)
+	{
+		return;
+	}
+	cout << a;
+	printLettersInReverse(a + 1, b);
+}
+
+int main() {
+	char slovo1, slovo2;
+	do
+	{
+		do
+		{
+			cout << "Unesite prvo slovo: ";
+			cin >> slovo1;
+			cout << "Unesite drugo slovo: ";
+			cin >> slovo2;
+		} while (!isupper(slovo1) || !isupper(slovo2));
+	} while (slovo1 > slovo2);
+
+	printLettersInReverse(slovo1, slovo2);
+}
