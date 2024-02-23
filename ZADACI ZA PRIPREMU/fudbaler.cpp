321ea04702eb13f53c5ee2e77c857bc54bdd8a06
Maid Petrović <126613894+rioDevv@users.noreply.github.com>
2024-02-23 18:57:41 +0100

    Add files via upload

---
+++ ZADACI ZA PRIPREMU/fudbaler.cpp
@@ -0,0 +1,83 @@
+﻿#include<iostream>
+using namespace std;
+
+struct fudbaler {
+	int* godRodjenja;
+	int* brojIgraca;
+	int* golovi;
+
+	fudbaler() {
+		godRodjenja = new int;
+		brojIgraca = new int;
+		golovi = new int;
+	}
+
+	~fudbaler() {
+		delete godRodjenja;
+		godRodjenja = nullptr;
+
+		delete brojIgraca;
+		brojIgraca = nullptr;
+
+		delete golovi;
+		golovi = nullptr;
+	}
+};
+
+void unos(fudbaler** matrix, int rows, int cols) {
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			*matrix[i][j].godRodjenja = 1980 + rand() % ((2000 + 1) - 1980);
+			*matrix[i][j].brojIgraca = rand() % 30 + 1;
+			*matrix[i][j].golovi = rand() % 5;
+		}
+	}
+}
+
+void ispis(fudbaler** matrix, int rows, int cols) {
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << "Godina rodjenja je: " << *matrix[i][j].godRodjenja << "\t";
+			cout << "Broj igraca je: " << *matrix[i][j].brojIgraca << "\t";
+			cout << "Broj golova igraca je: " << *matrix[i][j].golovi << endl;
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+	srand(time(0));
+
+	int rows;
+	int cols;
+
+	do
+	{
+		cout << "Unesite broj redova: ";
+		cin >> rows;
+		cout << "Unesite broj kolona: ";
+		cin >> cols;
+	} while (rows < 1 || cols < 1);
+
+	fudbaler** matrix = new fudbaler*[rows];
+
+	for (int i = 0; i < rows; i++)
+	{
+		matrix[i] = new fudbaler[cols];
+	}
+
+	unos(matrix, rows, cols);
+	ispis(matrix, rows, cols);
+
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] matrix[i];
+		matrix[i] = nullptr;
+	}
+	delete[] matrix;
+	matrix = nullptr;
+}
---
+++ ZADACI ZA PRIPREMU/vozilo.cpp
@@ -0,0 +1,95 @@
+﻿#include<iostream>
+using namespace std;
+
+struct vozilo {
+	char* markaVozila;
+	char* brojSasije;
+	char* tipVozila;
+	int* kubnihCM;
+
+	vozilo() {
+		markaVozila = new char[10];
+		brojSasije = new char[10];
+		tipVozila = new char;
+		kubnihCM = new int;
+	}
+
+	~vozilo() {
+		delete[] markaVozila;
+		markaVozila = nullptr;
+
+		delete[] brojSasije;
+		brojSasije = nullptr;
+
+		delete tipVozila;
+		tipVozila = nullptr;
+
+		delete kubnihCM;
+		kubnihCM = nullptr;
+	}
+};
+
+void unos(vozilo** niz, int rows, int cols) {
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cin.ignore();
+			cout << "Unesite marku vozila: ";
+			cin.getline(niz[i][j].markaVozila, 10);
+			cout << "Unesite broj sasije: ";
+			cin.getline(niz[i][j].brojSasije, 10);
+			do
+			{
+				cout << "Unesite tip vozila: ";
+				cin >> *niz[i][j].tipVozila;
+			} while (*niz[i][j].tipVozila != 'A' && *niz[i][j].tipVozila != 'B' && *niz[i][j].tipVozila != 'C');
+			cout << "Unesite broj kubne potrosnje vozila: ";
+			cin >> *niz[i][j].kubnihCM;
+		}
+	}
+}
+
+void ispis(vozilo** niz, int rows, int cols) {
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << "Marka vozila je: " << niz[i][j].markaVozila << "\t";
+			cout << "Broj sasije vozila je: " << niz[i][j].brojSasije << "\t";
+			cout << "Tip vozila je: " << *niz[i][j].tipVozila << "\t";
+			cout << "Kubna potrosnja vozila je: " << *niz[i][j].kubnihCM << endl;
+		}
+	}
+}
+
+int main() {
+	int rows;
+	int cols;
+
+	do
+	{
+		cout << "Unesite broj redova: ";
+		cin >> rows;
+		cout << "Unesite broj kolona: ";
+		cin >> cols;
+	} while (rows < 1 || cols < 1);
+
+	vozilo** niz = new vozilo * [rows];
+	
+	for (int i = 0; i < rows; i++)
+	{
+		niz[i] = new vozilo[cols];
+	}
+
+	unos(niz, rows, cols);
+	ispis(niz, rows, cols);
+
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] niz[i];
+		niz[i] = nullptr;
+	}
+	delete[] niz;
+	niz = nullptr;
+}
