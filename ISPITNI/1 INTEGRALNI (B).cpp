4c5b526a1b5c61819b8ab5fe23335c31ac4b9d28
Maid Petrović <126613894+rioDevv@users.noreply.github.com>
2024-02-23 19:12:51 +0100

    Add files via upload

---
+++ ISPITNI/1 INTEGRALNI (B).cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<iostream>
+using namespace std;
+
+int main() {
+	srand(time(0));
+
+	int karta1;
+	int karta2;
+	int karta3;
+	int brojPonavaljanja = 0;
+	int brojIteracija = 0;
+	
+	do
+	{
+		karta1 = rand() % 14 + 1;
+		karta2 = rand() % 14 + 1;
+		karta3 = rand() % 14 + 1;
+		brojIteracija++;
+		if (karta1 + 5 == karta2 && karta2 + 5 == karta3)
+		{
+			cout << karta1 << " " << karta2 << " " << karta3 << endl;
+			brojPonavaljanja++;
+		}
+	} while (brojPonavaljanja < 1);
+
+	cout << "Izvrsilo se nakon: " << brojIteracija << " ponavljanja." << endl;
+}
+
